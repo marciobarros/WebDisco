7c0d31c810075727dee0203e55211309d46a5fad
Marcio Barros <marcio.barros@uniriotec.br>
2014-10-17 15:36:46 -0300

    Uso de Lombok nas classes de modelo

--- 07 SimpleMVC/WebDiscoSimpleMVC/src/br/unirio/webdisco/model/CompactDisc.java
+++ 07 SimpleMVC/WebDiscoSimpleMVC/src/br/unirio/webdisco/model/CompactDisc.java
@@ -1,98 +1,29 @@
-/*
+package br.unirio.webdisco.model;
+
+import lombok.Data;
+
+/**
  * Classe que representa um CD
  *  
  * @author marcio.barros
  */
-
-package br.unirio.webdisco.model;
-
-public class CompactDisc
+public @Data class CompactDisc
 {
-	private int id;
-	private String title;
-	private double price;
-	private double stock;
-	
-	/**
-	 * Inicializa um CD 
-	 */
+	private int id = -1;
+	private String title = "";
+	private double price = 0.0;
+	private double stock = 1.0;
+
 	public CompactDisc() 
 	{
 		this(-1, "", 0.0, 1.0);
 	}
-	
-	/**
-	 * Inicializa um CD 
-	 */
+
 	public CompactDisc(int id, String title, double price, double stock) 
 	{
 		this.id = id;
 		this.title = title;
 		this.price = price;
-		this.stock = stock;
-	}
-	
-	/**
-	 * Retorna o ID do CD
-	 */
-	public int getId()
-	{
-		return id;
-	}
-	
-	/**
-	 * Muda o ID do CD
-	 */
-	public void setId(int id)
-	{
-		this.id = id;
-	}
-	
-	/**
-	 * Retorna o título do CD
-	 */
-	public String getTitle() 
-	{
-		return title;
-	}
-	
-	/**
-	 * Altera o título do CD
-	 */
-	public void setTitle(String title) 
-	{
-		this.title = title;
-	}
-	
-	/**
-	 * Retorna o preço do CD
-	 */
-	public double getPrice() 
-	{
-		return price;
-	}
-	
-	/**
-	 * Altera o preço do CD 
-	 */
-	public void setPrice(double price) 
-	{
-		this.price = price;
-	}
-	
-	/**
-	 * Retorna o número de CDs em estoque
-	 */
-	public double getStock() 
-	{
-		return stock;
-	}
-	
-	/**
-	 * Altera o número de CDs em estoque
-	 */
-	public void setStock(double stock) 
-	{
 		this.stock = stock;
 	}
 }
